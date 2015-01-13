6cf0dff03eaec8b15f584784584cfb9e52262636
sebastien ballesteros <sebastien.ballesteros@gmail.com>
2015-01-13 16:32:45 -0500

    serialize and restore selections

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # verse
 
-Verse for the web (deep linking). Inspired by https://github.com/NYTimes/Emphasis
+Verse for the web (deep linking). Inspired by
+https://github.com/NYTimes/Emphasis but leverages the
+[Range interface](https://developer.mozilla.org/en-US/docs/Web/API/Range)
+and
+[selection object](https://developer.mozilla.org/en-US/docs/Web/API/Selection).
+
+Identify a paragraph by
+
+- Breaking it into Sentences
+- Taking first and last Sentences (ok if that's the same)
+- taking the first character from the first three words of each sentence
+
+Such identifier has been shown to provide (for articles at least)
+
+- Uniqueness
+- Consistency
+- Tolerance
+
+From within a paragraph, a region can be referenced by using character
+offset.
+
+For instance the word ```sentences``` of the following paragraph
+
+    I am a paragraph with 2 sentences.
+    I am the second sentence.
+
+can be referred as:
+
+```IaaIat:25-33```
 
 
 ## Demo
--- index.js
+++ index.js
@@ -16,26 +16,15 @@ function createKey($p) {
   var txt = ($p.textContent || '').replace(/[^a-z\. ]+/gi, '').trim();
 
   if (txt && txt.length>1) {
+    //TODO use https://github.com/Tessmore/sbd or something similar
     var lines = txt
           .split('.')
           .map(function(x) {return x.trim();})
           .filter(function(x) {return x;});
 
     if (lines.length) {
-      var first = lines[0]
-            .replace(/[\s\s]+/gi, ' ')
-            .split(' ')
-            .slice(0, (len/2))
-            .map(function(x) {return x.trim();})
-            .filter(function(x) {return x;});
-
-      var last = lines[lines.length-1]
-            .replace(/[\s\s]+/gi, ' ')
-            .split(' ')
-            .slice(0, (len/2))
-            .map(function(x) {return x.trim();})
-            .filter(function(x) {return x;});
-
+      var first = lines[0].match(/\S+/g).slice(0, (len/2));
+      var last = lines[lines.length-1].match(/\S+/g).slice(0, (len/2));
       var k = first.concat(last);
 
       var max = (k.length>len) ? len : k.length;
@@ -49,7 +38,7 @@ function createKey($p) {
   return key;
 };
 
-
+//key:start-end
 function serializeSelection() {
 
   //TODO compute prefix and suffix: get all the text nodes, and know
@@ -79,32 +68,95 @@ function serializeSelection() {
 
   var key = createKey($scope);
 
-  var it = document.createNodeIterator($scope, NodeFilter.SHOW_ALL);
-  var node;
-  var i = 0;
-  var start, end;
-
-  while (node = it.nextNode()) {
-    if (node === range.startContainer){
-      start = i;
-    } else if (node === range.endContainer){
-      end = i;
+  var startTextNode, endTextNode;
+  if (range.startContainer.nodeType === Node.TEXT_NODE) {
+    startTextNode = range.startContainer;
+  } else {
+    for (var i=0; i<range.startContainer.childNodes.length; i++) {
+      if (range.startContainer.childNodes[i].nodeType === Node.TEXT_NODE) {
+        startTextNode = range.startContainer.childNodes[i];
+        break;
+      }
     }
-    i++;
+  }
+
+  if (range.endContainer.nodeType === Node.TEXT_NODE) {
+    endTextNode = range.endContainer;
+  } else {
+    for (var j=0; j<range.endContainer.childNodes.length; j++) {
+      if (range.endContainer.childNodes[j].nodeType === Node.TEXT_NODE) {
+        endTextNode = range.endContainer.childNodes[j];
+        break;
+      }
+    }
+  }
+
+  var node, indStartTextNode, indEndTextNode, ind=0, textNodes = [];
+  var it = document.createNodeIterator($scope, NodeFilter.SHOW_TEXT);
+  while (node = it.nextNode()) {
+    textNodes.push(node);
+    if (node === startTextNode) {
+      indStartTextNode = ind;
+    }
+    if (node === endTextNode) {
+      indEndTextNode = ind;
+      break;
+    }
+    ind++;
+  }
+
+  var startOffset = textNodes.slice(0, indStartTextNode).reduce(function(a, b){
+    return a + b.textContent.length;
+  }, 0);
+  if (range.startOffset !== undefined) {
+    startOffset += range.startOffset;
+  }
+
+  var endOffset = textNodes.slice(0, indEndTextNode).reduce(function(a, b){
+    return a + b.textContent.length;
+  }, 0);
+  if (range.endOffset !== undefined) {
+    endOffset += range.endOffset;
   }
 
   return {
+    $scope: $scope,
     sha1: crypto.createHash('sha1').update($scope.textContent.trim()).digest('hex'),
     key: createKey($scope),
-    start: start,
-    end: end,
-    startOffset: range.startOffset,
-    endOffset: range.endOffset,
+    startOffset: startOffset,
+    endOffset: endOffset,
     text: selection.toString()
   };
 
 };
 
+function rangeFromOffsets($scope, startOffset, endOffset) {
+  var node;
+  var it = document.createNodeIterator($scope, NodeFilter.SHOW_TEXT);
+  var acc = 0;
+  var startNode, endNode, relStartOffset, relEndOffset;
+
+  while (node = it.nextNode()) {
+    if (relStartOffset === undefined && ((acc + node.textContent.length) >= startOffset)) {
+      startNode = node;
+      relStartOffset = startOffset-acc;
+    }
+    if (relEndOffset === undefined && ((acc + node.textContent.length) >= endOffset)) {
+      endNode = node;
+      relEndOffset = endOffset-acc;
+      break;
+    }
+    acc += node.textContent.length;
+  }
+
+  var range = document.createRange();
+  range.setStart(startNode, relStartOffset);
+  range.setEnd(endNode, relEndOffset);
+
+  return range;
+};
+
 
 exports.createKey = createKey;
 exports.serializeSelection = serializeSelection;
+exports.rangeFromOffsets = rangeFromOffsets;
