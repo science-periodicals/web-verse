4e1e2fd4f21efd3795a5c389f39b885389d8b938
Robin Berjon <robin@berjon.com>
2015-09-29 11:27:08 -0400

    add configuration for karma

---
+++ karma.conf-ci.js
@@ -0,0 +1,153 @@
+
+var fs = require('fs')
+  , localConf = require('./karma.conf')
+;
+
+// karma for CI
+module.exports = function (config) {
+  // Use ENV vars on Travis and sauce.json locally to get credentials
+  if (!process.env.SAUCE_USERNAME) {
+    if (!fs.existsSync('sauce.json')) {
+      console.log('Create a sauce.json with your credentials based on the sauce-sample.json file.');
+      process.exit(1);
+    }
+    else {
+      process.env.SAUCE_USERNAME = require('./sauce.json').username;
+      process.env.SAUCE_ACCESS_KEY = require('./sauce.json').accessKey;
+    }
+  }
+  // XXX need to add quite a few more here
+  // Browsers to run on Sauce Labs
+  var customLaunchers = {
+    // chrome, two versions back
+    'sl_chrome-latest': {
+      base:         'SauceLabs',
+      browserName:  'chrome',
+      platform:     'Windows 7',
+      version:      '47'
+    },
+    'sl_chrome-1': {
+      base:         'SauceLabs',
+      browserName:  'chrome',
+      platform:     'OS X 10.9',
+      version:      '46'
+    },
+    'sl_chrome-2': {
+      base:         'SauceLabs',
+      browserName:  'chrome',
+      platform:     'Linux',
+      version:      '45'
+    },
+
+    // IE, back to IE9
+    'sl_ie-latest': {
+      base:         'SauceLabs',
+      browserName:  'internet explorer',
+      platform:     'Windows 10',
+      version:      '20'
+    },
+    'sl_ie-11': {
+      base:         'SauceLabs',
+      browserName:  'internet explorer',
+      platform:     'Windows 8.1',
+      version:      '11'
+    },
+    'sl_ie-10': {
+      base:         'SauceLabs',
+      browserName:  'internet explorer',
+      platform:     'Windows 8',
+      version:      '10'
+    },
+    'sl_ie-9': {
+      base:         'SauceLabs',
+      browserName:  'internet explorer',
+      platform:     'Windows 7',
+      version:      '9'
+    },
+
+    // Firefox, two versions back
+    'sl_firefox-latest': {
+      base:         'SauceLabs',
+      browserName:  'firefox',
+      platform:     'Windows 10',
+      version:      '41'
+    },
+    'sl_firefox-1': {
+      base:         'SauceLabs',
+      browserName:  'firefox',
+      platform:     'OS X 10.9',
+      version:      '40'
+    },
+    'sl_firefox-2': {
+      base:         'SauceLabs',
+      browserName:  'firefox',
+      platform:     'Linux',
+      version:      '39'
+    },
+
+    // Safari, desktop, two versions back
+    'sl_safari-latest': {
+      base:         'SauceLabs',
+      browserName:  'safari',
+      version:      '8'
+    },
+    'sl_safari-1': {
+      base:         'SauceLabs',
+      browserName:  'safari',
+      version:      '7'
+    },
+    'sl_safari-2': {
+      base:         'SauceLabs',
+      browserName:  'safari',
+      version:      '6'
+    },
+
+    // iOS, two versions back
+    'sl_ios-latest': {
+      base:         'SauceLabs',
+      browserName:  'iphone',
+      version:      '8'
+    },
+    'sl_ios-1': {
+      base:         'SauceLabs',
+      browserName:  'iphone',
+      version:      '7'
+    },
+    'sl_ios-2': {
+      base:         'SauceLabs',
+      browserName:  'iphone',
+      version:      '6'
+    },
+
+    // Android, down to 4.3
+    'sl_android-latest': {
+      base:         'SauceLabs',
+      browserName:  'android',
+      version:      '5.1'
+    },
+    'sl_android-1': {
+      base:         'SauceLabs',
+      browserName:  'android',
+      version:      '4.3'
+    },
+    'sl_android-2': {
+      base:         'SauceLabs',
+      browserName:  'android',
+      version:      '4.2'
+    }
+  };
+
+  localConf(config);
+  config.set({
+    sauceLabs: {
+      testName: 'Web Verse'
+    },
+    captureTimeout: 120000,
+    customLaunchers: customLaunchers,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: Object.keys(customLaunchers),
+    singleRun: true
+  });
+};
---
+++ karma.conf.js
@@ -0,0 +1,36 @@
+
+module.exports = function (config) {
+  config.set({
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+    // frameworks to use, from: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+    // list of files / patterns to load in the browser
+    files: [
+      'web-verse.min.js',
+      'test/*.js'
+    ],
+    // test results reporter to use, from https://npmjs.org/browse/keyword/karma-reporter
+    // possible values: 'dots', 'progress'
+    reporters: ['progress'],
+    // web server port
+    port: 9876,
+    colors: true,
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome', 'Firefox', 'Safari'],
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
+};
