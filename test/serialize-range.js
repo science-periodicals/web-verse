4a78da44865c28d29e272cd73ff867a0dd12a791
Robin Berjon <robin@berjon.com>
2015-10-01 11:55:07 -0400

    also test serializeSelection

--- test/serialize-range.js
+++ test/serialize-range.js
@@ -38,4 +38,27 @@ describe('WebVerse serializeRange', function () {
     assert.equal(res.endOffset, 11, 'normalised the end offset');
   });
 });
+describe('WebVerse serializeSelection', function () {
+  it('should ignore collapsed selections', function () {
+    var res = WebVerse.serializeSelection();
+    assert.equal(res, undefined, 'returned undefined');
+  });
+  it('should serialise a selection', function () {
+    htmlContent('<p id="x"> \n\t<span>hello</span>\n<span>world</span>  \n\f\v</p>');
+    var $p = document.getElementsByTagName('p')[0]
+      , range = document.createRange()
+    ;
+    range.setStart($p.firstChild, 3);
+    range.setEnd($p.childNodes.item(3).firstChild, 5);
+    var sel = window.getSelection();
+    sel.addRange(range);
+    var res = WebVerse.serializeSelection();
+    assert.equal(res.$scope, $p, 'defaulted the scope');
+    assert.equal(res.hash, hashedHW, 'hashed the content properly');
+    assert.equal(res.key, 'hwhw', 'generates the correct key');
+    assert.equal(res.startOffset, 0, 'normalised the start offset');
+    assert.equal(res.endOffset, 11, 'normalised the end offset');
+  });
+});
+
 })();
